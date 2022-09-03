b044403df619fd108a3b6a64d638e8566bca4145
OleksiiAfanasenko <112640981+OleksiiAfanasenko@users.noreply.github.com>
2022-09-03 16:12:22 +0300

    task3

--- main.py
+++ main.py
@@ -2,3 +2,11 @@ Eleks = ["Oleksii", "Maksim", "Viktor"]
 Toshiba = ["Oleksii", "Andrii", "Ruslan"]
 Toshiba.extend(Eleks)
 print(Toshiba)
+
+casino_blacklist = ['John Dow', 'John_Smith', 'John Lin', 'John Gol']
+poker_blacklist = ['John Dow', 'John Dic', 'John Bim', 'John Bol']
+alcohol_blacklist = ['John Dow', 'John Uil', 'John Won', 'John Pol']
+casino_blacklist.extend(poker_blacklist)
+casino_blacklist.extend(alcohol_blacklist)
+print(casino_blacklist.count('John Dow'))
+#не впевнен що вірно, бо якщо масив даних великий то дуже муторно шукати по одній людині, а список я не зміг вставити
