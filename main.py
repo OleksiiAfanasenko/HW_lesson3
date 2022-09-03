26413895e1b7534f9f76b192ea6eb3266e5c7145
OleksiiAfanasenko <112640981+OleksiiAfanasenko@users.noreply.github.com>
2022-09-03 16:13:00 +0300

    task4

--- main.py
+++ main.py
@@ -10,3 +10,12 @@ casino_blacklist.extend(poker_blacklist)
 casino_blacklist.extend(alcohol_blacklist)
 print(casino_blacklist.count('John Dow'))
 #не впевнен що вірно, бо якщо масив даних великий то дуже муторно шукати по одній людині, а список я не зміг вставити
+
+group_one = {"vegetables": "omnivores1",
+             "all": "omnivores2",
+            }
+group_two = {"vegetables": "vegetarian",
+            }
+print(group_one.get("vegetables"))
+print(group_two.get("vegetables"))
+
